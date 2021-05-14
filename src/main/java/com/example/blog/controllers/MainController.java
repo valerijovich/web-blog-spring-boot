a26da296b89b6c6add3c8adc19daae0e50ab7a26
valerijovich <paulvalerijovich@gmail.com>
2021-05-14 16:17:00 +0300

    web-blog-spring-boot-v1

---
+++ src/main/java/com/example/blog/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Page about us");
+        return "about";
+    }
+
+}
